1d5a1b5a85d85405148bbd4b4e6bfeb7e242b2e8
tsushiy <28962399+tsushiy@users.noreply.github.com>
2019-10-18 16:28:14 +0900

    Clean the code up

--- App.js
+++ App.js
@@ -1,34 +1,35 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Swiper from 'react-native-swiper';
-import {MainScreen} from './src/MainScreen.js';
-import {SettingScreen} from './src/SettingScreen.js';
+
+import { MainScreen } from './src/MainScreen.js';
+import { SettingScreen } from './src/SettingScreen.js';
+
 const styles = StyleSheet.create({
   slide: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#9DD6EB',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
   },
   text: {
-      color: '#FFFFFF',
-      fontSize: 30,
-      fontWeight: 'bold',
+    color: '#FFFFFF',
+    fontSize: 30,
+    fontWeight: 'bold',
   },
   Main: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#576071',
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#576071',
   },
   Setting: {
-      flex: 1,
-      justifyContent: 'center',
-      alignItems: 'center',
-      backgroundColor: '#afa598'
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#afa598'
   }
 })
-
 
 export default class App extends React.Component {
   render() {
--- src/SettingScreen.js
+++ src/SettingScreen.js
@@ -2,39 +2,39 @@ import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export class SettingScreen extends React.Component {
-    render() {
-        return (
-            <View style={styles.Setting}>
-                <Text style={styles.text}>
-                    ここで(場所×時間)→曲の設定をして欲しいdesy
-                </Text>
-            </View>
-        )
-    }
+  render() {
+    return (
+      <View style={styles.Setting}>
+        <Text style={styles.text}>
+          ここで(場所×時間)→曲の設定をして欲しいdesy
+        </Text>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
-    slide: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#9DD6EB',
-    },
-    text: {
-        color: '#FFFFFF',
-        fontSize: 30,
-        fontWeight: 'bold',
-    },
-    Main: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#576071',
-    },
-    Setting: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        backgroundColor: '#afa598'
-    }
+  slide: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#9DD6EB',
+  },
+  text: {
+    color: '#FFFFFF',
+    fontSize: 30,
+    fontWeight: 'bold',
+  },
+  Main: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#576071',
+  },
+  Setting: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#afa598'
+  }
 })
