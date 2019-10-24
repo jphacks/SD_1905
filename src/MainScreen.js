05247b820dcd2110e86f1fdffd6d2474c38dcaf2
koya_watanabe <w.kou.1012@gmai.com>
2019-10-24 13:34:09 +0900

    Add Deamon

--- src/MainScreen.js
+++ src/MainScreen.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, Alert } from 'react-native';
 import { Marker } from 'react-native-maps';
 import MapView, { PROVIDER_DEFAULT, PROVIDER_GOOGLE } from 'react-native-maps';
 import Geolocation from '@react-native-community/geolocation';
@@ -68,6 +68,45 @@ export class MainScreen extends React.Component {
         this.setState({ markers: newMarkers })
       })
       .catch(err => console.warn(err))
+  }
+  isNear(obj, c_lat, c_lng){
+    if(Math.abs( (obj.place.latitude-c_lat) < 0.00001) && (Math.abs(obj.place.longitude-c_lng)<0.00001 )){
+      return true;
+    }
+    else{
+      return false;
+    }
+  }
+  checker(){
+    // this.getCurrentPosition(this);
+    console.log('you\'re @ (latlng) '+this.state.latitude+'/'+this.state.longitude);
+    const c_lat=38.25451378490567;
+    const c_lng=140.84350658013643;
+    storage
+    .load({ key: 'mapInfo' })
+    .then(res => {
+      console.log('informations')
+      console.log(res)
+      res.map(obj => {
+        if( this.isNear(obj, c_lat, c_lng) ){
+          const musicId=obj.musicId;
+          this.setState({musicId:obj.musicId})
+          // musicPlay(obj.musicId)
+          // Alert.alert(obj.musicId)
+          console.log('hit!! '+obj.musicId)
+        }
+        else{
+          console.log('not hit ...')
+        }
+      })
+    })
+    .catch(err => console.warn(err)) 
+  }
+//  ComponentWillMountで初期化するらしい．調べてみたい．
+  componentDidMount(){
+    this.interval = setInterval(() => {
+      this.checker()
+    }, 5000);
   }
 
   render() {
